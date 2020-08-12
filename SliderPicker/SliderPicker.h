3b92b1d14bd1fcce81214f3f23e385f5a25c9aed
Orlov Roman <rorlov@onvis.ru>
2020-08-12 13:59:22 +0300

    Initial commit

---
+++ SliderPicker/SliderPicker.h
@@ -0,0 +1,19 @@
+//
+//  SliderPicker.h
+//  SliderPicker
+//
+//  Created by Роман Орлов on 12.08.2020.
+//  Copyright © 2020 Roman Orlov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SliderPicker.
+FOUNDATION_EXPORT double SliderPickerVersionNumber;
+
+//! Project version string for SliderPicker.
+FOUNDATION_EXPORT const unsigned char SliderPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SliderPicker/PublicHeader.h>
+
+
---
+++ SliderPickerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SliderPickerDemo
+//
+//  Created by Роман Орлов on 12.08.2020.
+//  Copyright © 2020 Roman Orlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
